ad0bf28716a65479770a7ba80c432eef87e7e7cf
An Vuong <anvuong1998@gmail.com>
2019-01-14 16:14:53 -0500

    hello
    hello

--- X-Ray/Data/xray_code_An.py
+++ X-Ray/Data/xray_code_An.py
@@ -21,17 +21,21 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import spinmob as sm
-peak1 = sm.data.load('Cu-100-09-01.UXD')
-peak2 = sm.data.load('Cu-75-Ni-25-09-01.UXD')
-peak3 = sm.data.load('Cu-50-Ni-50-09-01.UXD')
-peak4 = sm.data.load('Cu-25-Ni-75-10-01.UXD')
-peak5 = sm.data.load('Ni-100-09-01.UXD')
+import spinmob as s
+peak1 = s.data.load('Cu-100-09-01.UXD')
+peak2 = s.data.load('Cu-75-Ni-25-09-01.UXD')
+peak3 = s.data.load('Cu-50-Ni-50-09-01.UXD')
+peak4 = s.data.load('Cu-25-Ni-75-10-01.UXD')
+peak5 = s.data.load('Ni-100-09-01.UXD')
+alloy_legend = ["Cu100", "Cu75", "Cu50", "Cu25", "Cu0"]
 
-
-sm.plot.xy.data([peak1[0],peak2[0],peak3[0],peak4[0],peak5[0]],\
+s.plot.xy.data([peak1[0],peak2[0],peak3[0],peak4[0],peak5[0]],\
                 [peak1[1],peak2[1],peak3[1],peak4[1],peak5[1]],\
-                yshift = 100)
+                yshift = 150,\
+                xlabel = 'Angle 2theta',\
+                ylabel = 'Intensity [Counts]',\
+                label = alloy_legend,\
+                legend = 'right')
 
 
 
