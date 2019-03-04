1c85c9931dce7675eef952f2d64217f2ad5f585f
Elisa Jacquet <elisa.elena.jacquet@gmail.com>
2019-03-04 19:16:35 -0400

    worked on cropping

--- Fourier Optics Lab/CroppingImages.py
+++ Fourier Optics Lab/CroppingImages.py
@@ -16,14 +16,24 @@ from skimage import data, img_as_float, io, util
 from scipy import ndimage as ndi
 
 #get dot positions
-imgdots = io.imread("IMG_0748.jpg")[1500:3000,1500:4000]
+imgdots = io.imread("IMG_0750.jpg")[1500:3000,1750:4250]
 imdots = rgb2gray(imgdots)
 imdotsT = np.ndarray.transpose(imdots)
 avDot = np.empty(imdots[0].size)
 xdots = np.arange(0,imdots[0].size,1)
 #for 0739: [1500:3000,1500:4000] 
+#for 0741 : [1500:3000,1250:3750]
+#for 0742: [1500:3000,1750:4250]
+#for 0743: [1500:3000,1750:4250]
+#for 0744: 1500:3000,2250:4750]
+#for 0745: [1500:3000,1750:4250]
+#for 0747: [1500:3000,1500:4000]
+#for 0748: [1500:3000,1500:4000]
+#for 0749: [1500:3000,1500:4000]
+#for 0750: [1500:3000,1750:4250]
+#optimal crop size: [1400:3100, 1250:4750]
 
-for i in range(imdots[0].size):
+for i in range(imdots[0].size): 
     avDot[i] = np.average(imdotsT[i])
 
 
