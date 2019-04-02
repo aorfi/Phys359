11f85a0478880d44fa87a27e61452a301984bd4c
aorfi <aorfi1234@gmail.com>
2019-04-02 02:19:18 -0400

    Started the exponential fit for I0
    Need to figure out the correct constants

--- Compton Scattering/I0Determination .py
+++ Compton Scattering/I0Determination .py
@@ -8,6 +8,7 @@ Created on Mon Apr  1 17:48:09 2019
 import spinmob as s
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 dx = np.asarray(np.loadtxt('1brickX.txt', delimiter=' '))
 dy = np.asarray(np.loadtxt('1brickY.txt', delimiter=' '))
@@ -26,9 +27,29 @@ y3 = 20323.243740714344
 x4 = 365.22# 365.22 +/- 0.89
 y4 = 7093.71560223599
 
-x_values = [x3,x4,x2,x1]
-y_values = [y3,y4,y2,y1]
+x_values = [0.094,0.162,0.254,0.322]
+y_values = [y1,y2,y3,y4]
 
-s.plot.xy.data(x_values,y_values)
+ytest = np.zeros(4)
+for i in range (0,3):
+    ytest[i] = 1200000*math.exp(-3*x_values[i])
+    
 
+s.plot.xy.data([x_values,x_values],[y_values,ytest])
+
+f = s.data.fitter()
+a = 3466 #check this
+f.set_functions('I*e**(-3466*x)','I') 
+
+y_error = 100
+
+f.set_data(xdata = x_values, ydata = y_values , eydata = y_error)
+f.set(I = 1000)
+
+
+f.set(plot_guess = True, xlabel = 'thickness',
+      ylabel = 'I')
+
+f.fit()
+print(f)
     
